d62c40b9dba33cabcb3f752aec357779039a87fb
Javier Rojas <javierrojas62@gmail.com>
2025-06-05 12:12:01 -0300

    Updates

---
+++ precache-manifest.f6bb6fef3cdcdb04c1ab3a9f07eba0bd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2713f040283c3acc8d56b670ec2788d",
+    "url": "/tuMejorVersion/index.html"
+  },
+  {
+    "revision": "c5dd11e6a000115b0091",
+    "url": "/tuMejorVersion/static/css/2.de1632da.chunk.css"
+  },
+  {
+    "revision": "d05377e9dc9da45758eb",
+    "url": "/tuMejorVersion/static/css/main.db09039c.chunk.css"
+  },
+  {
+    "revision": "c5dd11e6a000115b0091",
+    "url": "/tuMejorVersion/static/js/2.df662fb2.chunk.js"
+  },
+  {
+    "revision": "b00714318c79406e8a6445ca11f057ed",
+    "url": "/tuMejorVersion/static/js/2.df662fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a6c0b8b3f8f95083c5",
+    "url": "/tuMejorVersion/static/js/3.4267c53f.chunk.js"
+  },
+  {
+    "revision": "d05377e9dc9da45758eb",
+    "url": "/tuMejorVersion/static/js/main.c2e3582c.chunk.js"
+  },
+  {
+    "revision": "5597f24a2464b8b2cb80",
+    "url": "/tuMejorVersion/static/js/runtime-main.d935ae7a.js"
+  },
+  {
+    "revision": "987a1f5ac2831f3d529bcfbb452648ce",
+    "url": "/tuMejorVersion/static/media/1.987a1f5a.png"
+  },
+  {
+    "revision": "83bdb515eaf305f352f967c2c1dccee1",
+    "url": "/tuMejorVersion/static/media/2.83bdb515.png"
+  },
+  {
+    "revision": "702c4ae2da25489bfcc5329693e17771",
+    "url": "/tuMejorVersion/static/media/3.702c4ae2.png"
+  },
+  {
+    "revision": "dc42b39b9090ec85cfd3d1ef1fd71c2a",
+    "url": "/tuMejorVersion/static/media/4.dc42b39b.png"
+  },
+  {
+    "revision": "e85af4b8d87da22a84e4fde6b16fef58",
+    "url": "/tuMejorVersion/static/media/logo.e85af4b8.png"
+  }
+]);
